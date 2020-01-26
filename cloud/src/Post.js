7630d5ced01c6b65816709bd3402ee42d91fa04d
Vanessa0122 <yp4@ualberta.ca>
2020-01-25 22:44:17 -0700

    switch it to class

--- cloud/src/Post.js
+++ cloud/src/Post.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-function Post(props){
-    return(
-        <div>
-            <h1>{props.post.id}</h1>
-            <h1>{props.post.content}</h1>
-        </div>
-    )
-
+class Post extends React.Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.props.post.id}</h1>
+                <h1>{this.props.post.content}</h1>
+            </div>
+        )
+    }
 }
 
 export default Post
