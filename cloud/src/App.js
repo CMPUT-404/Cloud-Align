89596a775ecf0b50095cffe0f2ca8294b89290b6
Joe <32971172+uzamakihina@users.noreply.github.com>
2020-01-27 23:27:10 -0800

    images

--- cloud/src/App.js
+++ cloud/src/App.js
@@ -3,13 +3,22 @@ import Post from './Post'
 import './App.css';
 import PostsData from './PostsData'
 
+import Face from './Components/Face.js'
+
 class App extends React.Component {
   render(){
     const PostCompnents = PostsData.map(item => <Post post={item}/>)
+    
+    const Temp = <Face />
+    
+    
   
     return (
       <div className="App">
+        {Temp}
         {PostCompnents}
+        
+
       </div>
     )
 
---
+++ cloud/src/Components/Face.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+
+class Face extends React.Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+
+            <div>
+            <img src={require('../Images/pepe.png')} />
+           </div>
+
+
+        )
+    }
+}
+export default Face
