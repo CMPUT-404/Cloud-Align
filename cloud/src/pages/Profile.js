18c045de90d3d1b8aebbc73d55e67faec35f62f7
joe <jlx@ualberta.ca>
2020-02-02 18:26:34 -0700

    databse

--- cloud/src/pages/Profile.js
+++ cloud/src/pages/Profile.js
@@ -20,7 +20,45 @@ class Profile extends React.Component {
 
   render(){ 
     
-   
+
+    // GET
+    var request = new XMLHttpRequest()
+    // request.open('GET','http://162.246.157.219:25565/users')
+    // request.send()
+
+    // var temp = null
+    // request.onload = ()=>{
+    //   temp = JSON.parse(request.response)
+    //   alert(temp[0].url)
+    // }
+
+    //POST 
+
+    // request.open('POST','http://162.246.157.219:25565/users/')
+    // request.setRequestHeader("Authorization", "Basic " + btoa("admin:123456"));
+    // request.setRequestHeader("Content-Type", "application/json")
+    
+
+
+    // request.onreadystatechange = function () {
+    //   alert(request.responseText)
+    //   if (request.readyState === 4 && request.status === 200) {
+    //       var json = JSON.parse(request.responseText);
+    //       alert(JSON.stringify(json));
+    //   }
+    // };
+
+
+    // request.send('{"username":"AtestCow","password":"123456"}')
+
+      
+
+  
+
+
+
+
+
     return (
       <div className="Profile" >
 
@@ -33,7 +71,7 @@ class Profile extends React.Component {
           <input type="text" placeholder="Email"></input><br></br>
           <input type="text" placeholder="PhoneNumber"></input><br></br>
           
-          <button> Save Changes </button>
+          <button> temp.users </button>
 
         </form>
         <button>
