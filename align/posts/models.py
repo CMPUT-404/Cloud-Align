62b696f03d5e6f43ea627d1d4709db118e4adaf7
TIANQI-PC\tqwan <twang2@ualberta.ca>
2020-02-16 14:51:00 -0700

    Create user model

--- align/posts/models.py
+++ align/posts/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.utils import timezone
-#from users.models import User
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 # Create your models here.
+User = get_user_model()
+
+
 class Posts(models.Model):
-    # it has a title
+	# it has a title
 	title = models.CharField(max_length=300)
-    # it has a author (many to one)
+	# it has a author (many to one)
 	author = models.ForeignKey(User, on_delete = models.CASCADE)
-    # it has a authority
-    # 0 = public
-    # 1 = friend only
-    # 2 = people who are assigned(by default yourself)
-    # 3 = private and only by urls
+	# it has a authority
+	# 0 = public
+	# 1 = friend only
+	# 2 = people who are assigned(by default yourself)
+	# 3 = private and only by urls
 	authorization = models.CharField(max_length=300)
 	plainText = models.TextField()
 	images = models.ImageField(blank=True)
