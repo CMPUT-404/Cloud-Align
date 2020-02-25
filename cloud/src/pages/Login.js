a5007bf3dc66ca5bdff5f2d85baf07c93a9aad4d
Vanessa0122 <yp4@ualberta.ca>
2020-02-25 12:18:11 -0700

    adds registration button

--- cloud/src/pages/Login.js
+++ cloud/src/pages/Login.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
-import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
-import { FacebookLoginButton } from 'react-social-login-buttons';
+import {FormGroup, Label, Input} from 'reactstrap';
 import './css/Login.css';
 
 class Login extends React.Component {
@@ -12,11 +11,13 @@ class Login extends React.Component {
         <h2>Welcome to Cloud Align</h2>
         <h3>Login</h3>
         <FormGroup class='login-form'>
-          <Label id='emailLabel'>Enter Your Email</Label>
+          <Label>Enter Your Email</Label>
           <Input id='emailInput' type='email' placeholder='Email'/>
-          <Label id='passwordLabel'>Enter Your Password</Label>
+          <Label>Enter Your Password</Label>
           <Input id='passwordInput' type='password' placeholder='Password'/>
-          <button id='submitButton'>login</button>
+          <Label>Don't have an account? Click on the register button</Label>
+          <button id='registerButton'>Register</button>
+          <button id='submitButton'>Login</button>
         </FormGroup>
       </div>
     )
