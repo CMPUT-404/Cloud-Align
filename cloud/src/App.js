15a177f98ad5937e409f3932200e0e94a5f9b863
Vanessa0122 <yp4@ualberta.ca>
2020-02-25 12:05:43 -0700

    adds login page, need to track state (isLoggedIn)

--- cloud/src/App.js
+++ cloud/src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import './App.css';
 import Friends from './Pages/Friends';
 import Profile from './Pages/Profile';
-import NavBar from './Components/NavBar'
-import Timeline from './Pages/Timeline'
+import NavBar from './Components/NavBar';
+import Timeline from './Pages/Timeline';
+import Login from './Pages/Login';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      isLoggedIn: false,
+    }
+  }
+
   render(){  
-    return (
-      <div className="App">
+
+    if (this.state.isLoggedIn===false){
+      return (
+        <Login />
+      )
+    } else {
+      return(
         <Router>
           <NavBar />
           <Switch>
@@ -17,9 +30,8 @@ class App extends React.Component {
             <Route exact path="/timeline" component={Timeline}/>
           </Switch>
         </Router>
-      </div>
-    )
-
+      );
+    }
   }
 }
 export default App
--- cloud/src/pages/Login.js
+++ cloud/src/pages/Login.js
@@ -1,87 +1,26 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
+import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import { FacebookLoginButton } from 'react-social-login-buttons';
+import './css/Login.css';
 
-export default class Registration extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      email: "",
-      password: "",
-      password_confirmation: "",
-      registrationErrors: ""
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    const { email, password, password_confirmation } = this.state;
-
-    axios
-      .post(
-        "http://localhost:3001/registrations",
-        {
-          user: {
-            email: email,
-            password: password,
-            password_confirmation: password_confirmation
-          }
-        },
-        { withCredentials: true }
-      )
-      .then(response => {
-        if (response.data.status === "created") {
-          this.props.handleSuccessfulAuth(response.data);
-        }
-      })
-      .catch(error => {
-        console.log("registration error", error);
-      });
-    event.preventDefault();
-  }
-
-  render() {
+class Login extends React.Component {
+  render(){  
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            required
-          />
-
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            required
-          />
-
-          <input
-            type="password"
-            name="password_confirmation"
-            placeholder="Password confirmation"
-            value={this.state.password_confirmation}
-            onChange={this.handleChange}
-            required
-          />
-
-          <button type="submit">Register</button>
-        </form>
+        <h2>Welcome to Cloud Align</h2>
+        <h3>Login</h3>
+        <FormGroup class='login-form'>
+          <Label id='emailLabel'>Enter Your Email</Label>
+          <Input id='emailInput' type='email' placeholder='Email'/>
+          <Label id='passwordLabel'>Enter Your Password</Label>
+          <Input id='passwordInput' type='password' placeholder='Password'/>
+          <button id='submitButton'>login</button>
+        </FormGroup>
       </div>
-    );
+    )
+
   }
 }
+export default Login
